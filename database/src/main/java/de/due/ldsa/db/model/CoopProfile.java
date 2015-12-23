e25e7a7acca74642992e6799d25e1df0aef177ac
scrobart <werwolf-krieger>
2015-12-23 14:52:08 +0100

    Updated CoopProfile according to the new model

--- database/src/main/java/de/due/ldsa/db/model/CoopProfile.java
+++ database/src/main/java/de/due/ldsa/db/model/CoopProfile.java
@@ -2,10 +2,36 @@ package de.due.ldsa.db.model;
 
 import de.due.ldsa.db.DbException;
 
+import java.time.LocalDate;
+
 /**
  *
  */
 public class CoopProfile extends Profile
 {
-    CoopKind kind;
+    LocalDate dateFounded;
+
+    public int countInteraction(Profile p)
+        throws DbException
+    {
+        throw new DbException("not yet implemented.");
+    }
+
+    public double countAverageInteractionPerFeed(Profile p)
+            throws DbException
+    {
+        throw new DbException("not yet implemented.");
+    }
+
+    public double getAverageInteractionPerFeed()
+        throws DbException
+    {
+        throw new DbException("not yet implemented.");
+    }
+
+    public double getAverageOfActionsPerDay()
+        throws DbException
+    {
+        throw new DbException("not yet implemented.");
+    }
 }
