090176b6313c30a379af69780ba49f48a3eb3034
Vincent Nelius <vincent.nelius@stud.uni-due.de>
2016-02-04 18:56:22 +0100

    BDW-88
    scaffolded basic angularjs-bootstrap webapp

---
+++ webservice-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webserviceFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webserviceFrontendApp
+ */
+angular.module('webserviceFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ webservice-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webserviceFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webserviceFrontendApp
+ */
+angular.module('webserviceFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
