135c65181b303639ae2fdf15a2b04942dc47dc05
Vincent Nelius <Vincent Nelius>
2016-02-07 22:13:04 +0100

    BDW-88
    Switched from angular native routeProvider to routeSegmentProvider
    Fixed issue with tracking service menu item (referencing an empty site)
    Added sub-pages for tracking service requests

--- webservice-frontend/app/scripts/app.js
+++ webservice-frontend/app/scripts/app.js
@@ -14,34 +14,70 @@ angular
     'ngCookies',
     'ngResource',
     'ngRoute',
+	'route-segment',
+	'view-segment',
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-	  .when('/analysis', {
-		templateUrl: 'views/analysis.html'
+  .config(function($routeSegmentProvider, $routeProvider) {
+    $routeSegmentProvider.options.autoLoadTemplates = true;
+	$routeSegmentProvider
+      .when('/main', 'main')
+      .when('/about', 'about')
+	  .when('/analysis', 'analysis')
+	  .when('/database', 'database')
+	  .when('/request', 'request')
+	  .when('/request/users', 'request.users')
+	  .when('/request/relationships', 'request.relationships')
+	  .when('/request/media', 'request.media')
+	  .when('/request/comments', 'request.comments')
+	  .when('/request/likes', 'request.likes')
+	  .when('/request/tags', 'request.tags')
+	  .when('/request/locations', 'request.locations')
+	  .when('/token', 'token')
+	  .segment('main', {
+		default: true, 
+		templateUrl: 'views/main.html',
+        controller: 'MainCtrl'  
 	  })
-	  .when('/database',{
-		templateUrl: 'views/database.html'
+	  .segment('about', {
+		templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'  
 	  })
-	  .when('/request',{
-		templateUrl: 'views/request.html'
+	  .segment('analysis', {
+		templateUrl: 'views/analysis.html'  
 	  })
-	  .when('/token',{
-		templateUrl: 'views/token.html'
+	  .segment('database', {
+		templateUrl: 'views/database.html'  
 	  })
-      .otherwise({
-        redirectTo: '/'
-      });
+	  .segment('request', {
+		templateUrl: 'views/request.html'  
+	  })
+	  .within()
+		.segment('users', {
+			templateUrl: 'views/users.html'
+		})
+		.segment('relationships', {
+			templateUrl: 'views/relationships.html'
+		})
+		.segment('media', {
+			templateUrl: 'views/media.html'
+		})
+		.segment('comments', {
+			templateUrl: 'views/comments.html'
+		})
+		.segment('likes', {
+			templateUrl: 'views/likes.html'
+		})
+		.segment('tags', {
+			templateUrl: 'views/tags.html'
+		})
+		.segment('locations', {
+			templateUrl: 'views/locations.html'
+		})
+		.up()
+	  .segment('token', {
+		templateUrl: 'views/token.html'  
+	  });
+	  $routeProvider.otherwise({redirectTo: '/main'});
   });
