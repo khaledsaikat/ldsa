aa75b9a8540ffe97d98b48c9278fc60553988742
scrobart <werwolf-krieger>
2016-03-12 15:02:36 +0100

    Changed ProfileFeed to used long instead of int (see BDW-92)

--- system-model/src/main/java/de/due/ldsa/model/ProfileFeed.java
+++ system-model/src/main/java/de/due/ldsa/model/ProfileFeed.java
@@ -45,9 +45,9 @@ public class ProfileFeed extends SocialNetworkContentImpl {
 	@Column(name = "links")
 	ArrayList<URL> links;
 	@Column(name = "locationId")
-	int locationId;
+	long locationId;
 	@Column(name = "mediaId")
-	int mediaId;
+	long mediaId;
 	@Column(name = "taggedUserIds")
 	ArrayList<Long> taggedUserIds;
 	@Column(name = "commentIds")
@@ -125,19 +125,19 @@ public class ProfileFeed extends SocialNetworkContentImpl {
 		this.links = links;
 	}
 
-	public int getLocationId() {
+	public long getLocationId() {
 		return locationId;
 	}
 
-	public void setLocationId(int locationId) {
+	public void setLocationId(long locationId) {
 		this.locationId = locationId;
 	}
 
-	public int getMediaId() {
+	public long getMediaId() {
 		return mediaId;
 	}
 
-	public void setMediaId(int mediaId) {
+	public void setMediaId(long mediaId) {
 		this.mediaId = mediaId;
 	}
 
@@ -177,37 +177,28 @@ public class ProfileFeed extends SocialNetworkContentImpl {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof ProfileFeed))
-			return false;
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
 
 		ProfileFeed that = (ProfileFeed) o;
 
-		if (socialNetworkId != that.socialNetworkId)
-			return false;
-		if (id != that.id)
-			return false;
-		if (profileId != that.profileId)
-			return false;
-		if (locationId != that.locationId)
-			return false;
-		if (mediaId != that.mediaId)
-			return false;
+		if (socialNetworkId != that.socialNetworkId) return false;
+		if (id != that.id) return false;
+		if (profileId != that.profileId) return false;
+		if (locationId != that.locationId) return false;
+		if (mediaId != that.mediaId) return false;
 		if (contentTimestamp != null ? !contentTimestamp.equals(that.contentTimestamp) : that.contentTimestamp != null)
 			return false;
-		if (crawlingTimestamp != null ? !crawlingTimestamp.equals(that.crawlingTimestamp)
-				: that.crawlingTimestamp != null)
+		if (crawlingTimestamp != null ? !crawlingTimestamp.equals(that.crawlingTimestamp) : that.crawlingTimestamp != null)
 			return false;
-		if (!rawStoryText.equals(that.rawStoryText))
+		if (rawStoryText != null ? !rawStoryText.equals(that.rawStoryText) : that.rawStoryText != null) return false;
+		if (likerIds != null ? !likerIds.equals(that.likerIds) : that.likerIds != null) return false;
+		if (sharerIds != null ? !sharerIds.equals(that.sharerIds) : that.sharerIds != null) return false;
+		if (hashtagNames != null ? !hashtagNames.equals(that.hashtagNames) : that.hashtagNames != null) return false;
+		if (links != null ? !links.equals(that.links) : that.links != null) return false;
+		if (taggedUserIds != null ? !taggedUserIds.equals(that.taggedUserIds) : that.taggedUserIds != null)
 			return false;
-		if (likerIds != null ? !likerIds.equals(that.likerIds) : that.likerIds != null)
-			return false;
-		if (sharerIds != null ? !sharerIds.equals(that.sharerIds) : that.sharerIds != null)
-			return false;
-		if (hashtagNames != null ? !hashtagNames.equals(that.hashtagNames) : that.hashtagNames != null)
-			return false;
-		return !(links != null ? !links.equals(that.links) : that.links != null) && !(taggedUserIds != null ? !taggedUserIds.equals(that.taggedUserIds) : that.taggedUserIds != null) && !(commentIds != null ? !commentIds.equals(that.commentIds) : that.commentIds != null);
+		return !(commentIds != null ? !commentIds.equals(that.commentIds) : that.commentIds != null);
 
 	}
 
@@ -218,16 +209,15 @@ public class ProfileFeed extends SocialNetworkContentImpl {
 		result = 31 * result + (crawlingTimestamp != null ? crawlingTimestamp.hashCode() : 0);
 		result = 31 * result + (int) (id ^ (id >>> 32));
 		result = 31 * result + (int) (profileId ^ (profileId >>> 32));
-		result = 31 * result + rawStoryText.hashCode();
+		result = 31 * result + (rawStoryText != null ? rawStoryText.hashCode() : 0);
 		result = 31 * result + (likerIds != null ? likerIds.hashCode() : 0);
 		result = 31 * result + (sharerIds != null ? sharerIds.hashCode() : 0);
 		result = 31 * result + (hashtagNames != null ? hashtagNames.hashCode() : 0);
 		result = 31 * result + (links != null ? links.hashCode() : 0);
-		result = 31 * result + locationId;
-		result = 31 * result + mediaId;
+		result = 31 * result + (int) (locationId ^ (locationId >>> 32));
+		result = 31 * result + (int) (mediaId ^ (mediaId >>> 32));
 		result = 31 * result + (taggedUserIds != null ? taggedUserIds.hashCode() : 0);
 		result = 31 * result + (commentIds != null ? commentIds.hashCode() : 0);
 		return result;
 	}
-
 }
